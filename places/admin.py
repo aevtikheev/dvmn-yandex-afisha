b72a82bbe36ab2c6809f0991027f00285808ba52
aevtikheev <eft000@gmail.com>
2020-12-15 22:33:32 +0300

    Add image inline for companies

--- places/admin.py
+++ places/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from places.models import Company, Image
 
 
-admin.site.register(Company)
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
+
 admin.site.register(Image)
