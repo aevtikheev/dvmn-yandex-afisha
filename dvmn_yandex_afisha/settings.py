93e018a59290304a3c15a5adc7cb051a884b30af
aevtikheev <eft000@gmail.com>
2020-12-15 23:33:35 +0300

    MOve DEBUG and SECRET_KEY settings to env variables

--- dvmn_yandex_afisha/settings.py
+++ dvmn_yandex_afisha/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p8o9x-*!#+4s8bye@+#x99*f^xgyx&=n&4@%yu=ylet&gn=dh3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
-
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
