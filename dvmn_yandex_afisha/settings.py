54d25c82902bf9b490c659c53c18d65b7c2d1af3
aevtikheev <eft000@gmail.com>
2020-12-18 00:39:34 +0300

    Add default database name setting

--- dvmn_yandex_afisha/settings.py
+++ dvmn_yandex_afisha/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'dvmn_yandex_afisha.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / os.getenv('DATABASE_NAME'),
+        'NAME': BASE_DIR / os.getenv('DATABASE_NAME', 'db.sqlite3'),
     }
 }
 
